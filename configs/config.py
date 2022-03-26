605a02c5424233b97c20862e4538f1acc33c3278
WycliffeMuchumi <muchumimaina94@gmail.com>
2022-03-26 15:37:13 +0300

    deploying updates

--- configs/config.py
+++ configs/config.py
@@ -7,7 +7,7 @@
 
 
 class Production:
-    SQLALCHEMY_DATABASE_URI = 'postgres://blufcyfuephvbf:f6bb9cce21036c899c21ea893eb19ef5568e2ef2c22316547c6ee5f3f149206a@ec2-176-34-184-174.eu-west-1.compute.amazonaws.com:5432/d36lve8t356t1v'
+    SQLALCHEMY_DATABASE_URI = 'postgres://vegyyixqcaeopk:558ba3166ccfc3fbb08244a69f9c10289840ece23e12199106abee3e9a5f50d2@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dep8r0o8d5cdmg'
     DEBUG = False
     #pass
 
