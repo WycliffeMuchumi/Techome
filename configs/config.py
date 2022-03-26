6d20c4a4fffc5ee3a9a612ac8d08f9aaf976962f
WycliffeMuchumi <muchumimaina94@gmail.com>
2022-03-26 16:11:40 +0300

    deploying updates

--- configs/config.py
+++ configs/config.py
@@ -7,7 +7,8 @@
 
 
 class Production:
-    SQLALCHEMY_DATABASE_URI = 'postgres://vegyyixqcaeopk:558ba3166ccfc3fbb08244a69f9c10289840ece23e12199106abee3e9a5f50d2@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dep8r0o8d5cdmg'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://vegyyixqcaeopk:558ba3166ccfc3fbb08244a69f9c10289840ece23e12199106abee3e9a5f50d2@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dep8r0o8d5cdmg'
+    SECRET_KEY = '558ba3166ccfc3fbb08244a69f9c10289840ece23e12199106abee3e9a5f50d2'
     DEBUG = False
     #pass
 
