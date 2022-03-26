feb0a1d151ae2e2520ebebfc624b0cbe15d5b748
WycliffeMuchumi <muchumimaina94@gmail.com>
2022-03-26 14:23:20 +0300

    deploying updates

--- configs/config.py
+++ configs/config.py
@@ -4,11 +4,12 @@ class Development:
     # DEBUG = True  # since we are in development mode
     # SECRET_KEY = "12121994"
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    Pass
 
 
 class Production:
-    QLALCHEMY_DATABASE_URI = 'postgres://blufcyfuephvbf:f6bb9cce21036c899c21ea893eb19ef5568e2ef2c22316547c6ee5f3f149206a@ec2-176-34-184-174.eu-west-1.compute.amazonaws.com:5432/d36lve8t356t1v'
-    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgres://blufcyfuephvbf:f6bb9cce21036c899c21ea893eb19ef5568e2ef2c22316547c6ee5f3f149206a@ec2-176-34-184-174.eu-west-1.compute.amazonaws.com:5432/d36lve8t356t1v'
+    DEBUG = False
     #pass
 
 
